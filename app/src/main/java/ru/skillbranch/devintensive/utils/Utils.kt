cf73dfd2fc6f0e1b9223e2adc017042668c6b11b
Trueshka <61644088+Trueshka@users.noreply.github.com>
2020-03-03 11:19:20 +0300

    hometask_2 complete

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -23,7 +23,7 @@ object Utils {
 
 
     fun transliteration(payload: String, devider: String = " "): String {
-        var state = ""
+      /*  var state = ""
         for (i in payload) {
             state += when (i.toString().toLowerCase(Locale.getDefault())) {
                 "а" -> "a"
@@ -62,13 +62,72 @@ object Utils {
                 else -> i
             }
         }
-        val parts: List<String>? = state.split(" ")
-        var firstName = parts?.getOrNull(0)
-        var lastName = parts?.getOrNull(1)
-       state = firstName!!.capitalize() + devider + lastName!!.capitalize()
+        var fin=""
 
-      // return parts?.joinToString(separator = devider) { it.capitalize() }.toString()
-        return state
+        var parts: List<String>? = state.split(" ")
+        var firstName = parts!!.getOrNull(0)?.capitalize()
+     //   parts.first() = parts.first().capitalize()
+       // var lastName = parts?.last()
+
+        /* state = firstName!!.capitalize() + devider + lastName!!.capitalize() */
+        var count=0
+for(i in parts){
+    if(count!=0&&count!=parts?.last())
+}
+       return parts?.joinToString(separator = devider) {  }.toString()
+        //return state*/
+         var state = ""
+            for (i in payload) {
+                state += when (i.toString().toLowerCase(Locale.getDefault())) {
+                    "а" -> "a"
+                    "б" -> "b"
+                    "в" -> "v"
+                    "г" -> "g"
+                    "д" -> "d"
+                    "е" -> "e"
+                    "ё" -> "e"
+                    "ж" -> "zh"
+                    "з" -> "z"
+                    "и" -> "i"
+                    "й" -> "i"
+                    "к" -> "k"
+                    "л" -> "l"
+                    "м" -> "m"
+                    "н" -> "n"
+                    "о" -> "o"
+                    "п" -> "p"
+                    "р" -> "r"
+                    "с" -> "s"
+                    "т" -> "t"
+                    "у" -> "u"
+                    "ф" -> "f"
+                    "х" -> "h"
+                    "ц" -> "c"
+                    "ч" -> "ch"
+                    "ш" -> "sh"
+                    "щ" -> "sh"
+                    "ъ" -> ""
+                    "ы" -> "i"
+                    "ь" -> ""
+                    "э" -> "e"
+                    "ю" -> "yu"
+                    "я" -> "ya"
+                    else -> i
+                }
+            }
+            var fin=""
+
+            var parts: List<String>? = state.split(" ")
+         //   var firstName = parts!!.getOrNull(0)?.capitalize()
+         //   parts.first() = parts.first().capitalize()
+           // var lastName = parts?.last()
+
+            /* state = firstName!!.capitalize() + devider + lastName!!.capitalize() */
+
+
+           return parts?.joinToString(separator = devider).toString()
+            //return state
+
     }
 
     fun toInitials(firstName: String?, lastName: String?): String? {
